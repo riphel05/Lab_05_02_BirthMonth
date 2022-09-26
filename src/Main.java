eca5e33fd043c1dc9088a80ef88a2b66cd884873
riphel05 <phelpsre@mail.uc.edu>
2022-09-25 22:01:40 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class
+Main {
+    public static void main(String[] args)
+    {
+        int birthMonth;
+        int userBirth = 10;
+
+        if(birthMonth <= 12);
+        {
+            System.out.println("Your birth month is " + userBirth);
+
+        }
+        else
+        {
+            System.out.println("You entered an incorrect birth month please re-enter ");
+        }
+    }
+}
